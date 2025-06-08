03a9d69b2165680063800db4e9bbaef6a80efcc6
maxchuk89 <maxchuk89@gmail.com>
2025-06-08 19:28:05 +0300

    починил

--- print_salary_tables.py
+++ print_salary_tables.py
@@ -1,15 +1,24 @@
-from hh_api import calculate_average_salaries as calculate_hh
-from sj_api import calculate_average_salaries_superjob as calculate_sj
-from print_salary_tables import print_salary_table
+import os
+from dotenv import load_dotenv
+
+from hh_api import calculate_average_salaries
+from sj_api import calculate_average_salaries_superjob, SJ_DEVELOPMENT_CATALOGUE_ID, SJ_PAGE_SIZE
+from salary_table import print_salary_table
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    superjob_key = os.getenv('SUPERJOB_API_KEY')
+
     languages = ['Python', 'Java', 'C++', 'C#', 'JavaScript', 'Ruby', 'Go', '1C']
 
-    hh_stats = calculate_hh(languages)
+    hh_stats = calculate_average_salaries(languages)
     print_salary_table(hh_stats, 'HeadHunter Moscow')
 
-    print()
-
-    sj_stats = calculate_sj(languages)
+    sj_stats = calculate_average_salaries_superjob(
+        languages,
+        superjob_key,
+        SJ_DEVELOPMENT_CATALOGUE_ID,
+        SJ_PAGE_SIZE
+    )
     print_salary_table(sj_stats, 'SuperJob Moscow')
